0336588bdab2a0e5899904c9daa741fee8a18507
Hemant <hemantyadav1137@gmail.com>
2024-08-31 23:24:32 +0530

    cors issue resolved

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
@@ -16,4 +18,8 @@ app.use("/api/v1", user);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Todo App");
 });
